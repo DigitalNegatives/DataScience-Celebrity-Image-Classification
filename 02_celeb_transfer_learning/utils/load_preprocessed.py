65db8b9479bc1c454ba669a12cdc4a3c0d4b7978
ANTON WIRSCH <Anton@ANTONs-MacBook-Pro.local>
2017-05-17 03:02:44 -0400

    Modified load_preprocess_data function. Updated main_inception_2step load_preprocessed_data call.

--- 02_celeb_transfer_learning/utils/load_preprocessed.py
+++ 02_celeb_transfer_learning/utils/load_preprocessed.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 import re
 
-def load_preprocessed_data(corpus_dir, bottles_dir, img_dims, apply_gaussian=False):
+# def load_preprocessed_data(corpus_dir, bottles_dir, img_dims, apply_gaussian=False):
+def load_preprocessed_data(corpus_dir, bottles_dir):
 
     entities = [f for f in os.listdir(corpus_dir) if re.match(r'm\.*', f)]
     entities.sort()
@@ -31,39 +32,40 @@ def load_preprocessed_data(corpus_dir, bottles_dir, img_dims, apply_gaussian=Fal
         labels_accumulated += [entity]*len(images)
 
 
-    if apply_gaussian:
-        print("Applied Gaussian")
-        Xs = gaussian(image_np_list, img_dims)
-    else:
-        Xs = np.array(image_np_list)
+    # if apply_gaussian:
+    #     print("Applied Gaussian")
+    #     Xs = gaussian(image_np_list, img_dims)
+    # else:
+    #     Xs = np.array(image_np_list)
 
+    Xs = np.array(image_np_list)
     Ys = np.array(labels_accumulated)
     Zs = np.array(bottle_np_list)
     return Xs, Ys, Zs
 
 
-def gaussian(images, img_dims):
-    x = np.linspace(-3.0, 3.0, img_dims)
-    mu = 0.0
-    sigma = 2.5
-
-    z = (np.exp(np.negative(np.power(x - mu, 2.0) /
-                       (2.0 * np.power(sigma, 2.0)))) *
-         (1.0 / (sigma * np.sqrt(2.0 * 3.1415))))
-
-    g2d = np.matmul(z.reshape(z.shape[0], 1), z.reshape(1, z.shape[0]))
-    g2d = g2d  / g2d.max()
-    g2d_3 = np.dstack((g2d,g2d,g2d))
-
-    normalized_images_list = []
-
-    for input_img in images:
-        # this_image = input_img - mean_image.reshape(img_dims, img_dims, 3)
-
-        masked_img = np.multiply(input_img, g2d_3)
-        #masked_img /= 255   #scale between 0 and 1
-        normalized_images_list.append(masked_img)
-
-    normalized_images = np.array(normalized_images_list)
-
-    return normalized_images
+# def gaussian(images, img_dims):
+#     x = np.linspace(-3.0, 3.0, img_dims)
+#     mu = 0.0
+#     sigma = 2.5
+#
+#     z = (np.exp(np.negative(np.power(x - mu, 2.0) /
+#                        (2.0 * np.power(sigma, 2.0)))) *
+#          (1.0 / (sigma * np.sqrt(2.0 * 3.1415))))
+#
+#     g2d = np.matmul(z.reshape(z.shape[0], 1), z.reshape(1, z.shape[0]))
+#     g2d = g2d  / g2d.max()
+#     g2d_3 = np.dstack((g2d,g2d,g2d))
+#
+#     normalized_images_list = []
+#
+#     for input_img in images:
+#         # this_image = input_img - mean_image.reshape(img_dims, img_dims, 3)
+# 
+#         masked_img = np.multiply(input_img, g2d_3)
+#         #masked_img /= 255   #scale between 0 and 1
+#         normalized_images_list.append(masked_img)
+#
+#     normalized_images = np.array(normalized_images_list)
+#
+#     return normalized_images
